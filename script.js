dbeffa55b3094de6441576d9e0f318c27a90f66b
01-Deepak <128479629+01-Deepak@users.noreply.github.com>
2024-01-15 20:13:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	const parsedDate1 = new Date(date1);
+    const parsedDate2 = new Date(date2);
+
+    // Calculate the time difference in milliseconds
+    const timeDiff = Math.abs(parsedDate2 - parsedDate1);
+
+    // Calculate the difference in days
+    const diffDays = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+
+    return diffDays;
 };
 
 // Do not change the code below.
